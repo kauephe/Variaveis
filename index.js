d37ea0a6d148ba347be4ad89d61b0182b230698b
kauephe <165967721+kauephe@users.noreply.github.com>
2024-09-17 22:52:24 +0000

    17/09/24

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 const nomeCompleto = "Kaue Phelipe";
 const anoNacimento = 2005;
 let anoAtual = 2024;
@@ -29,7 +32,8 @@ console.log(listaDeViagens);
 listaDeViagens.splice(1,1);
 console.log(listaDeViagens);
 
-const idadeComprador = 24;
+var nomeComprador = entrada("Digite seu nome: ");
+var idadeComprador = entrada("qual é a sua idade? ");
 
 if(idadeComprador < 18){
   console.log("Não altorisado voce é menor de idade")
